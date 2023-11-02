3ee00105a1159ba04e723d7c043e98548ab590ba
finestcat <125952040+finestcat@users.noreply.github.com>
2023-11-02 11:38:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
-const mongourl = ''; 
+const mongourl = 'mongodb+srv://alligator:<wanker125125>@cluster0.cyrgbvc.mongodb.net/?retryWrites=true&w=majority'; 
 const dbName = 'test';
 
 const express = require('express');
